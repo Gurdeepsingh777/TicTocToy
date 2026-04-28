374f93288a29cf60596417267e5523d256b95d86
Gurdeep-Codes <139622728+Gurdeepsingh777@users.noreply.github.com>
2026-04-28 23:46:48 +0530

    first commit

---
+++ app/src/main/java/com/cheater/tictoctoy/MainActivity.java
@@ -0,0 +1,83 @@
+package com.cheater.tictoctoy;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9;
+    String b1,b2,b3,b4,b5,b6,b7,b8,b9;
+    int flag=0;
+    int count =0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+    }
+    private void init (){
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+
+    }
+
+    public void Check(View view) {
+        Button curbtn = (Button) view;
+        count ++;
+        if (flag==0){
+            curbtn.setText("X");
+            flag=1;
+        }
+        else {
+            curbtn.setText("O");
+            flag=0;
+        }
+
+        if (count>4){
+            b1 = btn1.getText().toString();
+            b2 = btn2.getText().toString();
+            b3 = btn3.getText().toString();
+            b4 = btn4.getText().toString();
+            b5 = btn5.getText().toString();
+            b6 = btn6.getText().toString();
+            b7 = btn7.getText().toString();
+            b8 = btn8.getText().toString();
+            b9 = btn9.getText().toString();
+
+            if (b1.equals(b2)&&b2.equals(b3)&&!b1.equals("")){
+                Toast.makeText(context:this,text:"Winner is"+b1,Toast:LENGTH_SHORT).show();
+
+            } else if (b4.equals(b5)&&b5.equals(b6)&&!b4.equals("")) {
+                
+            } else if (b7.equals(b8)&&b8.equals(b9)&&!b7.equals("")) {
+                
+            } else if (b1.equals(b4)&&b4.equals(b7)&&!b1.equals("")) {
+                
+            } else if (b2.equals(b5)&&b5.equals(b8)&&!b2.equals("")) {
+                
+            } else if (b3.equals(b6)&&b6.equals(b9)&&!b3.equals("")) {
+                
+            } else if (b1.equals(b5)&&b5.equals(b9)&&!b1.equals("")) {
+                
+            } else if (b3.equals(b5)&&b5.equals(b7)&&!b3.equals("")) {
+                
+            }
+        }
+
+    }
+}
